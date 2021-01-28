3da4d469d1dc68e6203be151f8058f2ed1294610
Bratt <isaac1805@hotmail.com>
2021-01-28 15:00:57 -0500

    Todos los botones funcionan

--- js/main.js
+++ js/main.js
@@ -10,3 +10,29 @@ $('.nav a').on('click', function (e) {
             }, 800);
     }
 });
+
+$('.hero__container a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body')
+            .animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+    }
+});
+
+$('.contenedor a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body')
+            .animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+    }
+});
