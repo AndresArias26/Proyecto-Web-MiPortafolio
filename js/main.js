0361ef808c403300f8de309a163c98d0dbbac1b3
Bratt <isaac1805@hotmail.com>
2021-01-28 18:01:33 -0500

    Todos los botones funcionan optimizados.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
-$('.nav a').on('click', function (e) {
-    if (this.hash !== '') {
-        e.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body')
-            .animate({
-                scrollTop: $(hash).offset().top
-            }, 800);
-    }
-});
-
-$('.hero__container a').on('click', function (e) {
-    if (this.hash !== '') {
-        e.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body')
-            .animate({
-                scrollTop: $(hash).offset().top
-            }, 800);
-    }
-});
-
-$('.contenedor a').on('click', function (e) {
+$('a').on('click', function (e) {
     if (this.hash !== '') {
         e.preventDefault();
 
